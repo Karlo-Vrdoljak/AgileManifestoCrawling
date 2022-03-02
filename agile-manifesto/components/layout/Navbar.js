0406826f06f88128f60b297ee61bb9cd2cbe7c28
Karlo Vrdoljak <40829367+Karlo-Vrdoljak@users.noreply.github.com>
2022-03-02 09:52:41 +0100

    finished

--- agile-manifesto/components/layout/Navbar.js
+++ agile-manifesto/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="w-full text-xl sm:text-2xl md:text-3xl flex top-0 items-center  bg-sky-600 p-4 shadow-lg z-50 fixed shadow-sky-500/50 text-sky-100">
-      <div>Agile Manifesto</div>
+      <div>The Agile Manifesto</div>
     </div>
   );
 }
